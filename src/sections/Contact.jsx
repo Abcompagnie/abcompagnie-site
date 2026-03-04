c5fdfbb8fff14d154677d776d63cbb61319f163d
momooba07 <momooba07@gmail.com>
2026-03-04 01:52:57 +0000

    Ajout du lien de la page linkedin

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -28,9 +28,9 @@ export default function Footer() {
           </p>
 
           <div className="flex gap-4 mt-6">
-            <a className="hover:text-green-500" href="https://www.linkedin.com/company/abcompagnie"><Linkedin size={20} /></a>
-            <a className="hover:text-green-500" href="#"><FaTiktok size={20} /></a>
-            <a className="hover:text-green-500" href="https://web.facebook.com/profile.php?id=61588185046252"><Facebook size={20} /></a>
+            <a className="hover:text-[#BF391D]" href="https://www.linkedin.com/company/abcompagnie"><Linkedin size={20} /></a>
+            <a className="hover:text-[#BF391D]" href="#"><FaTiktok size={20} /></a>
+            <a className="hover:text-[#BF391D]" href="https://web.facebook.com/profile.php?id=61588185046252"><Facebook size={20} /></a>
           </div>
         </div>
 
